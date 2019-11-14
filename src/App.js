76882b644790a6df3a0297bcb489e01e000e186b
Jamie Aquino <jamie.aquino33@gmail.com>
2019-11-14 12:13:15 -0600

    Final push. Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import './App.css';
 import RecipeSearcher from './components/RecipeSearcher/RecipeSearcher';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import About from './About/About';
 
 
 function App() {
   return (
-    <div className="App">
-      <RecipeSearcher />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to ='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* The Switch component looks through the Route components inside of it and renders the first one that matches the current URL */}
+      <Switch>
+        <Route exact path='/'>
+          <RecipeSearcher />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
